62795f272c879a4dac700f4e592fd82c9d4ff6cb
Marc Schickedanz <marc.schickedanz@pivotfox.de>
2019-05-02 19:16:22 +0200

    new Folderstructure

---
+++ scripts/vps-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#file should be run as root
+
+#Generate SWAP
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce
+usermod -aG docker $USER
+
+ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
+docker run -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
